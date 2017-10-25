71ff8a37525f209900a9c5d491393fb6da2f6273
shuiyd <2503417117@qq.com>
2017-10-25 15:06:19 +0800

    Update server.go
    fix genNum error

--- viphxin/xingo/fserver/server.go
+++ viphxin/xingo/fserver/server.go
@@ -1,162 +1,179 @@
 package fserver
 
 import (
-	"fmt"
-	"github.com/viphxin/xingo/fnet"
-	"github.com/viphxin/xingo/iface"
-	"github.com/viphxin/xingo/logger"
-	"github.com/viphxin/xingo/timer"
-	"github.com/viphxin/xingo/udpserv"
-	"github.com/viphxin/xingo/utils"
-	"net"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
+        "fmt"
+        "github.com/viphxin/xingo/fnet"
+        "github.com/viphxin/xingo/iface"
+        "github.com/viphxin/xingo/logger"
+        "github.com/viphxin/xingo/timer"
+        "github.com/viphxin/xingo/udpserv"
+        "github.com/viphxin/xingo/utils"
+        "net"
+        "os"
+        "os/signal"
+        "syscall"
+        "time"
 )
 
 func init() {
-	utils.GlobalObject.Protoc = fnet.NewProtocol()
-	// --------------------------------------------init log start
-	utils.ReSettingLog()
-	// --------------------------------------------init log end
+        utils.GlobalObject.Protoc = fnet.NewProtocol()
+        // --------------------------------------------init log start
+        utils.ReSettingLog()
+        // --------------------------------------------init log end
 }
 
 type Server struct {
-	Port          int
-	MaxConn       int
-	GenNum        uint32
-	connectionMgr iface.Iconnectionmgr
-	timeChan      *timer.Timer
+        Port          int
+        MaxConn       int
+        GenNum        uint32
+        sessIdPool    chan uint32
+        connectionMgr iface.Iconnectionmgr
+        timeChan      *timer.Timer
 }
 
 func NewServer() iface.Iserver {
-	s := &Server{
-		Port:          utils.GlobalObject.TcpPort,
-		MaxConn:       utils.GlobalObject.MaxConn,
-		connectionMgr: fnet.NewConnectionMgr(),
-	}
-	utils.GlobalObject.TcpServer = s
+        s := &Server{
+                Port:          utils.GlobalObject.TcpPort,
+                MaxConn:       utils.GlobalObject.MaxConn,
+                connectionMgr: fnet.NewConnectionMgr(),
+        }
+        utils.GlobalObject.TcpServer = s
 
-	return s
+        return s
+}
+
+func (this *Server) initSessIdPool() {
+        this.sessIdPool = make(chan uint32, 4294967295)
+        ct := uint32(0)
+        for {
+                this.sessIdPool <- ct
+                ct++
+                if ct == 4294967295 {
+                        break
+                }
+        }
 }
 
 func (this *Server) handleConnection(conn *net.TCPConn) {
-	this.GenNum += 1
-	conn.SetNoDelay(true)
-	conn.SetKeepAlive(true)
-	// conn.SetDeadline(time.Now().Add(time.Minute * 2))
-	fconn := fnet.NewConnection(conn, this.GenNum, utils.GlobalObject.Protoc)
-	fconn.Start()
+        //this.GenNum += 1
+        if len(this.sessIdPool) < 1 {
+                this.initSessIdPool()
+        }
+        this.GenNum = <-this.sessIdPool
+        conn.SetNoDelay(true)
+        conn.SetKeepAlive(true)
+        // conn.SetDeadline(time.Now().Add(time.Minute * 2))
+        fconn := fnet.NewConnection(conn, this.GenNum, utils.GlobalObject.Protoc)
+        fconn.Start()
 }
 
 func (this *Server) Start() {
-	utils.StartFastSec()
-	go func() {
-		utils.GlobalObject.Protoc.InitWorker(utils.GlobalObject.PoolSize)
-		if utils.GlobalObject.IsGate() {
-			utils.GlobalObject.ProtocGate.InitWorker(utils.GlobalObject.PoolSize)
-		}
-		ln, err := net.ListenTCP("tcp", &net.TCPAddr{
-			Port: this.Port,
-		})
-		if err != nil {
-			//logger.Error(err)
-			panic(err)
-		}
-		logger.Info("start xingo server...")
-		for {
-			conn, err := ln.AcceptTCP()
-			if err != nil {
-				logger.Error(err)
-			}
-			logger.Info("get conn: %v", conn)
-			//max client exceed
-			if this.connectionMgr.Len() >= utils.GlobalObject.MaxConn {
-				conn.Close()
-			} else {
-				go this.handleConnection(conn)
-			}
-		}
-	}()
+        utils.StartFastSec()
+        go func() {
+                utils.GlobalObject.Protoc.InitWorker(utils.GlobalObject.PoolSize)
+                if utils.GlobalObject.IsGate() {
+                        utils.GlobalObject.ProtocGate.InitWorker(utils.GlobalObject.PoolSize)
+                }
+                ln, err := net.ListenTCP("tcp", &net.TCPAddr{
+                        Port: this.Port,
+                })
+                if err != nil {
+                        //logger.Error(err)
+                        panic(err)
+                }
+                logger.Info("start xingo server...")
+                for {
+                        conn, err := ln.AcceptTCP()
+                        if err != nil {
+                                logger.Error(err)
+                        }
+                        logger.Info("get conn: %v", conn)
+                        //max client exceed
+                        if this.connectionMgr.Len() >= utils.GlobalObject.MaxConn {
+                                conn.Close()
+                        } else {
+                                go this.handleConnection(conn)
+                        }
+                }
+        }()
 }
 
 func (this *Server) GetConnectionMgr() iface.Iconnectionmgr {
-	return this.connectionMgr
+        return this.connectionMgr
 }
 
 func (this *Server) GetConnectionQueue() chan interface{} {
-	return nil
+        return nil
 }
 
 func (this *Server) Stop() {
-	logger.Info("stop xingo server!!!")
+        logger.Info("stop xingo server!!!")
 
-	if utils.GlobalObject.IsGate() {
-		udpserv.GlobalUdpServ.Close()
-	}
-	if utils.GlobalObject.OnServerStop != nil {
-		utils.GlobalObject.OnServerStop()
-	}
+        if utils.GlobalObject.IsGate() {
+                udpserv.GlobalUdpServ.Close()
+        }
+        if utils.GlobalObject.OnServerStop != nil {
+                utils.GlobalObject.OnServerStop()
+        }
 }
 
 func (this *Server) AddRouter(router interface{}) {
-	logger.Info("AddRouter")
-	utils.GlobalObject.Protoc.GetMsgHandle().AddRouter(router)
+        logger.Info("AddRouter")
+        utils.GlobalObject.Protoc.GetMsgHandle().AddRouter(router)
 }
 
 func (this *Server) CallLater(durations time.Duration, f func(v ...interface{}), args ...interface{}) {
-	delayTask := timer.NewTimer(durations, f, args)
-	delayTask.Run()
+        delayTask := timer.NewTimer(durations, f, args)
+        delayTask.Run()
 }
 
 func (this *Server) CallWhen(ts string, f func(v ...interface{}), args ...interface{}) {
-	loc, err_loc := time.LoadLocation("Local")
-	if err_loc != nil {
-		logger.Error(err_loc)
-		return
-	}
-	t, err := time.ParseInLocation("2006-01-02 15:04:05", ts, loc)
-	now := time.Now()
-	if err == nil {
-		if now.Before(t) {
-			this.CallLater(t.Sub(now), f, args...)
-		} else {
-			logger.Error("CallWhen time before now")
-		}
-	} else {
-		logger.Error(err)
-	}
+        loc, err_loc := time.LoadLocation("Local")
+        if err_loc != nil {
+                logger.Error(err_loc)
+                return
+        }
+        t, err := time.ParseInLocation("2006-01-02 15:04:05", ts, loc)
+        now := time.Now()
+        if err == nil {
+                if now.Before(t) {
+                        this.CallLater(t.Sub(now), f, args...)
+                } else {
+                        logger.Error("CallWhen time before now")
+                }
+        } else {
+                logger.Error(err)
+        }
 }
 
 /*
 func (this *Server) CallLoop(msec uint32, f func(v ...interface{}), args ...interface{}) {
-	utils.GlobalObject.Timer.CreateTimer(int64(msec), f, args, true)
+        utils.GlobalObject.Timer.CreateTimer(int64(msec), f, args, true)
 }
 
 */
 
 func (this *Server) CallLoop(durations time.Duration, f func(v ...interface{}), args ...interface{}) {
-	go func() {
-		delayTask := timer.NewTimer(durations, f, args)
-		for {
-			time.Sleep(delayTask.GetDurations())
-			utils.GlobalObject.TimeChan <- delayTask
-		}
-	}()
+        go func() {
+                delayTask := timer.NewTimer(durations, f, args)
+                for {
+                        time.Sleep(delayTask.GetDurations())
+                        utils.GlobalObject.TimeChan <- delayTask
+                }
+        }()
 }
 
 func (this *Server) WaitSignal() {
-	// close
-	c := make(chan os.Signal, 1)
-	//signal.Notify(c, os.Interrupt, os.Kill)
-	signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGUSR1)
-	sig := <-c
-	logger.Info(fmt.Sprintf("server exit. signal: [%s]", sig))
-	this.Stop()
+        // close
+        c := make(chan os.Signal, 1)
+        //signal.Notify(c, os.Interrupt, os.Kill)
+        signal.Notify(c, os.Interrupt, os.Kill, syscall.SIGUSR1)
+        sig := <-c
+        logger.Info(fmt.Sprintf("server exit. signal: [%s]", sig))
+        this.Stop()
 }
 
 func (this *Server) Serve() {
-	this.Start()
-	this.WaitSignal()
+        this.Start()
+        this.WaitSignal()
 }
